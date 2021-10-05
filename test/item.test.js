dfd1584e3bd891d69830aec5a60bc8d24882f72f
poojithullasa <upsy.0515@gmail.com>
2021-10-05 17:04:54 +0530

    Added Mock Test Cases

--- test/item.test.js
+++ test/item.test.js
@@ -1,5 +1,8 @@
 const request = require("supertest");
 const app = require("../app");
+const MockAdapter = require("axios-mock-adapter");
+const axios = require("axios");
+const mock = new MockAdapter(axios);
 
 beforeAll(async () => {
   await request(app).post("/table/create");
@@ -30,5 +33,37 @@ describe("POST /item/add", () => {
     );
     const res = JSON.parse(response.text);
     expect(res.message).toBe("Sorry, Cannot Add Item");
+  });
+});
+
+describe("GET /item/read", () => {
+  test("Can we read item from the table?", async () => {
+    const response = await request(app).get("/item/read/2021&Love Story");
+    const res = JSON.parse(response.text);
+    expect(res.data.Item.title).toBe("Love Story");
+  });
+});
+
+describe("GET /item/contains", () => {
+  const usersUri = "/item/contains";
+  const url = new RegExp(`${usersUri}/*`);
+  const output = {
+    message: "Successfully got all items",
+    data: [
+      {
+        title: "Love Story",
+        year: 2021,
+        plot: "A Feel Good Movie",
+        ratings: 10,
+      },
+    ],
+  };
+  mock.onGet(url).replyOnce(200, output);
+  test("Can we get all items matching the containing title?", async () => {
+    const response = await axios({
+      method: "GET",
+      url: "/item/contains/Iron",
+    });
+    expect(response.data.data[0].title).toBe("Love Story");
   });
 });
--- test/table.test.js
+++ test/table.test.js
@@ -1,5 +1,8 @@
 const request = require("supertest");
 const app = require("../app");
+const axios = require("axios");
+const MockAdapter = require("axios-mock-adapter");
+const mock = new MockAdapter(axios);
 
 afterAll(async () => {
   await request(app).delete("/table/delete");
@@ -15,5 +18,60 @@ describe("POST /table/create", () => {
     const response = await request(app).post("/table/create");
     const res = JSON.parse(response.text);
     expect(res.message).toBe("Sorry, Cannot Create Table");
+  });
+});
+
+describe("DELETE /table/delete", () => {
+  mock
+    .onDelete("/table/delete")
+    .replyOnce(200, { message: "Sorry, Cannot Delete Table" })
+    .onDelete("/table/delete")
+    .replyOnce(200, { message: "Successfully Deleted Table" })
+    .onDelete("/table/delete")
+    .replyOnce(204);
+
+  test("Is it throwing error if table is not present?", async () => {
+    const response = await axios({
+      method: "DELETE",
+      url: "/table/delete",
+    });
+    expect(response.data.message).toBe("Sorry, Cannot Delete Table");
+  });
+  test("Is it successfully deleting table?", async () => {
+    const response = await axios({
+      method: "DELETE",
+      url: "/table/delete",
+    });
+    expect(response.data.message).toBe("Successfully Deleted Table");
+  });
+  test("Is it handling 404 error?", async () => {
+    const response = await axios({
+      method: "DELETE",
+      url: "/table/delete",
+    });
+    expect(response.status).toBe(204);
+  });
+});
+
+describe("GET /table/getall", () => {
+  mock
+    .onGet("/table/getall")
+    .replyOnce(204)
+    .onGet("/table/getall")
+    .replyOnce(200, { message: "Success" });
+  test("is Getall api is throwing 204 status code?", async () => {
+    const response = await axios({
+      method: "GET",
+      url: "/table/getall",
+    });
+    expect(response.status).toBe(204);
+  });
+  test("is Getall api is sending success message?", async () => {
+    const response = await axios({
+      method: "GET",
+      url: "/table/getall",
+    });
+    // const res = JSON.parse(response.);
+    expect(response.data.message).toBe("Success");
   });
 });
